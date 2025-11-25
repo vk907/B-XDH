301b4c5d4cced4775e7549bcf82d2fa41337c899
vk907 <44519978+vk907@users.noreply.github.com>
2025-11-25 14:09:34 +0900

    Refactor LWE parameters and estimation in test_pql.py

--- test_pql.py
+++ test_pql.py
@@ -1,12 +1,18 @@
 from estimator.lwe_parameters import LWEParameters as LWEest
 from estimator import *
-
+import math
+etas=4
+etae=4
+sig=2*math.sqrt(etae/2)
+Q=18433
+N=512
 params = LWEest(
-     n=768,
-     q=12289,#1280,256 bit
-     Xs=ND.CenteredBinomial(2),
-     Xe=ND.CenteredBinomial(4),
-     m=768,
+     n=N,
+     q=Q,#1280,256 bit
+     Xs=ND.CenteredBinomial(etas), # s
+     Xe=ND.DiscreteGassuian(sig), # 2e
+     m=N,
      tag="example"
 )
 r=LWE.estimate(params)
+
