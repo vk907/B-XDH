bdcabefce365b341c3e78ec2cba51c4fe795b9f3
vk907 <44519978+vk907@users.noreply.github.com>
2026-07-02 12:04:32 +0900

    Refactor LWE parameter estimation in test_pql.py
    Updated LWE parameter estimation for classical and quantum models.

--- test_pql.py
+++ test_pql.py
@@ -25,14 +25,13 @@ dual                 :: rop: ≈2^137.3, mem: ≈2^88.1, m: 512, β: 379, d: 102
 dual_hybrid          :: rop: ≈2^129.1, red: ≈2^129.0, guess: ≈2^125.5, β: 349, p: 6, ζ: 10, t: 30, β': 355, N: ≈2^73.5, m: 512
 """
 
-from estimator.reduction import MATZOV
-r=LWE.estimate(params,red_cost_model=MATZOV(nn="list_decoding-naive_quantum"))#quantum
-"""
-bkw                  :: rop: ≈2^180.9, m: ≈2^166.0, mem: ≈2^160.1, b: 11, t1: 0, t2: 19, ℓ: 10, #cod: 453, #top: 3, #test: 56, tag: coded-bkw
-usvp                 :: rop: ≈2^117.0, red: ≈2^117.0, δ: 1.004250, β: 364, d: 1020, tag: usvp
-bdd                  :: rop: ≈2^113.9, red: ≈2^113.1, svp: ≈2^112.6, β: 348, η: 384, d: 1023, tag: bdd
-dual                 :: rop: ≈2^121.1, mem: ≈2^88.9, m: 512, β: 376, d: 1024, ↻: 1, tag: dual
-dual_hybrid          :: rop: ≈2^115.7, red: ≈2^115.6, guess: ≈2^112.7, β: 354, p: 5, ζ: 0, t: 40, β': 366, N: ≈2^75.8, m: 512
-"""
-
+params = LWEest(
+     n=N,
+     q=Q,#1280,256 bit
+     Xs=ND.DiscreteGaussian(sig), # s0+s1
+     Xe=ND.DiscreteGaussian(2*sig), # 2e0+2e1
+     m=N,
+     tag="example"
+)
+r=LWE.estimate(params)#classical for prekis
 
